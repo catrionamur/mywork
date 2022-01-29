1f07ea1fb6fb1b201d2021d5f3b432d05dd7d10e
Catriona Murray <g00063047gmit.ie>
2022-01-29 17:56:20 +0000

    My Work

---
+++ Arith.py
@@ -0,0 +1,28 @@
+p1 = 2 +4
+p2 = 2+4
+#p3 = 2 2+4
+p4a = 8 - 5
+p4b = 21 * 55
+#p4c = round((21/4), 3)
+p4c = 21/4
+p4d = 21//4
+p4e = 21%4
+p4f = 2**8
+p5 = 2==3
+p6 = 2!=3
+p7a = 3>3
+p7b = 3>=3
+#p4d = round(21/4)
+print ("First Problem is", p1)
+print ("Second Problem is", p2)
+#print ("third Problem is a syntax error", p3)
+print("4A is", p4a)
+print("4b is", p4b)
+print("4c is", p4c)
+print("4d is", p4d)
+print("4e is", p4e)
+print("4f is", p4f)
+print("Is 2 equal to 3 :", p5)
+print("Is 2 not equal to 3 :", p6)
+print("Is 3 geater than 3 :", p7a)
+print("Is 3 geater than or equal to 3 :", p7b)
