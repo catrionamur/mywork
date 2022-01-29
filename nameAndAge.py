7a672cf10b688610252d9e2b46bd6e9becd42f03
Catriona Murray <g00063047gmit.ie>
2022-01-29 21:33:54 +0000

    Week 2

--- nameAndAge.py
+++ nameAndAge.py
@@ -1,5 +1,5 @@
 # nameAndAge.py
-# This program asks you for your name and age and outputs your name and age
+# This program asks you for your name and age then outputs your name and age
 # author: Catriona Murray
 name = input("Enter your name:")
 age = input("Enter your age:")
--- subtract.py
+++ subtract.py
@@ -1,5 +1,5 @@
 # subtract.py
-# This program demonstrates subtraction and concatenation of string
+# This program demonstrates subtraction and concatenation of a string
 # author: Catriona Murray
 x = (21 - 4)
 print ("X is equal to " + str(x))
