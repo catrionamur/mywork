e1e28a577e3e6a2be4539602059357e7cf1fc3c7
Catriona Murray <g00063047gmit.ie>
2022-02-05 16:16:47 +0000

    Week 3

---
+++ sub.py
@@ -0,0 +1,12 @@
+#sub.py
+# Week 3 : Program to subtract on number from another.
+# input reads in a string so we need to convert it into an int
+# so we can perform mathematical operations5
+# author: Catriona Murray
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+answer= x-y
+print("{} minus {} is {} ".format(x, y, answer))
+
+
