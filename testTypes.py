5d628325149385bc0f8ebb1c8f4ef4ed8ab3f083
Catriona Murray <g00063047gmit.ie>
2022-02-05 16:06:17 +0000

    test

---
+++ testTypes.py
@@ -0,0 +1,13 @@
+# testTypes.py
+# Playing around with variables of different types
+# author: Catriona Murray
+i = 3
+fl = 3.5
+isa = True
+memo = 'how now Brown Cow'
+lots = []
+print('variable {} is of type:{} and value:{}'.format('i', type(i), i))
+print('variable {} is of type:{} and value:{}'.format('fl', type(fl), fl))
+print('variable {} is of type:{} and value:{}'.format('is', type(isa), isa))
+print('variable {} is of type:{} and value:{}'.format('memo', type(memo), memo))
+print('variable {} is of type:{} and value:{}'.format('lots', type(lots), lots))
